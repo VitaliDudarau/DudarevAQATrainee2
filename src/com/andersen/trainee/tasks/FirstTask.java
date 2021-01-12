93c1a7bce997c6e010bd3b6156c7f9c91f0481d0
VitaliDudarau <dudarev.vitalii@gmail.com>
2021-01-13 00:28:28 +0300

    Add FirstTask.java

---
+++ src/com/andersen/trainee/tasks/FirstTask.java
@@ -0,0 +1,25 @@
+/*
+Составить алгоритм: если введенное число больше 7, то вывести “Привет”
+*/
+package com.andersen.trainee.tasks;
+
+import java.util.Scanner;
+
+public class FirstTask {
+
+    @SuppressWarnings("resources")
+    public static void main(String[] args) {
+
+        int a;
+
+        Scanner s =  new Scanner(System.in);
+        System.out.println("write a:");
+        a = s.nextInt();
+
+        if (a > 7) {
+            System.out.println("Привет");
+        } else {
+            System.out.println("You wrote a number less than or equal to 7");
+        }
+    }
+}
